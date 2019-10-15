df688201e7cf8db2edce1fb18f6c6fed3e5fb38b
Erika Foin <erika.foin@gmail.com>
2019-10-14 17:44:47 -0700

    first project plan in place

---
+++ plan.md
@@ -0,0 +1,11 @@
+Overall plan for BusMall Project
+
+1. Create products.js to hold array of products with applicable information.
+2. Create the following functions for use in creating app in product-array.js
+    1. GetProductById
+    2. RemoveProductById
+    3. DoesProductExist
+    4. GetRandomProduct
+3. Create tests for each function in products.test.js
+4. Create HTML for product page
+5. CSS styling for product page
---
+++ products-api.js
@@ -0,0 +1,7 @@
+export const productData = [
+    {
+        id: 'big-leaf-maple',
+        name: 'Big Leaf Maple',
+        image: '../assets/trees/big-leaf-maple.jpg',
+        description: 'Acer macrophyllum, the bigleaf maple or Oregon maple, is a large deciduous tree in the genus Acer. It can grow up to 157.80 feet (48.10 m) tall, but more commonly reaches 15–20 m (50–65 ft) tall. It is native to western North America, mostly near the Pacific coast, from southernmost Alaska to southern California.'
+    },
