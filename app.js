17c4fa413b93b283d3e845ddd8dadbe3244aedc4
Erika Foin <erika.foin@gmail.com>
2019-10-15 18:05:14 -0700

    ready to go home and it still does not work

--- app.js
+++ app.js
@@ -5,22 +5,22 @@ const img1 = document.getElementById('img-1');
 const img2 = document.getElementById('img-2');
 const img3 = document.getElementById('img-3');
 const productRadioTags = document.querySelectorAll('input');
-const productName = document.querySelectorAll('product-name');
+// const productName = document.querySelectorAll('product-name');
 const products = new ProductData(productData);
-let randomProduct1 = products.getRandomProduct();
-let randomProduct2 = products.getRandomProduct(); 
-let randomProduct3 = products.getRandomProduct();
+let randomProduct1 = products.getRandomProducts();
+let randomProduct2 = products.getRandomProducts(); 
+let randomProduct3 = products.getRandomProducts();
 
-        while (randomProduct1 === randomProduct2) {
-            randomProduct2 = products.getRandomProduct();
-        }
-        while (randomProduct3 === products.getRandomProduct() || randomProduct3 === randomProduct1) {
-            randomProduct3 = products.getRandomProduct();
-        }
+while (randomProduct1 === randomProduct2) {
+    randomProduct2 = products.getRandomProducts();
+}
+while (randomProduct3 === products.getRandomProducts() || randomProduct3 === randomProduct1) {
+    randomProduct3 = products.getRandomProducts();
+}
 
-        img1.src = getRandomProduct1.image;
-        img2.src = getRandomProduct2.image;
-        img3.src = getRandomProduct3.image;
+img1.src = randomProduct1.image;
+img2.src = randomProduct2.image;
+img3.src = randomProduct3.image;
 
         // productImageTags.forEach((imageTag, i) => {
         //     if (i === 0) {
@@ -32,16 +32,14 @@ let randomProduct3 = products.getRandomProduct();
         //         imageTag.src = randomProduct3.image;
         //     }
         // });
-        productRadioTags.forEach((radioTag, i) => {
-            if (i === 0) {
-                radioTag.value = randomProduct.id;
-            } else if (i === 1) {
-                radioTag.value = randomProduct2.id;
-            } else if (i === 2) {
-                radioTag.value = randomProduct3.id;
-            }
-            console.log(radioTag.value);
-        });
+productRadioTags.forEach((radioTag, i) => {
+    if (i === 0) {
+        radioTag.value = randomProduct1.id;
+    } else if (i === 1) {
+        radioTag.value = randomProduct2.id;
+    } else if (i === 2) {
+        radioTag.value = randomProduct3.id;
     }
-    generateProducts();
-};
+    console.log(radioTag.value);
+});
+    
