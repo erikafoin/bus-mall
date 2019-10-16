a73175a3a8e92243cce1d1f4125814b949390006
Erika Foin <erika.foin@gmail.com>
2019-10-16 09:37:21 -0700

    undid all coding done this morning to restore to displaying images

--- app.js
+++ app.js
@@ -1,27 +1,58 @@
 import { productData } from './products.js';
-import { ProductData } from '/bus-mall/ProductArray.js';
+import { ProductData } from '/ProductArray.js';
 
 const img1 = document.getElementById('img-1');
 const img2 = document.getElementById('img-2');
 const img3 = document.getElementById('img-3');
 const productRadioTags = document.querySelectorAll('input');
 // const productName = document.querySelectorAll('product-name');
+const choiceContainer = document.getElementById('product-choice-container');
+const nextContainer = document.getElementById('next-container');
+const nextButton = document.getElementById('next-button');
 const products = new ProductData(productData);
 let randomProduct1 = products.getRandomProducts();
 let randomProduct2 = products.getRandomProducts(); 
 let randomProduct3 = products.getRandomProducts();
 
-// productRadioTags.forEach(radioTag) => {
-//     radioTag.addEventListener('click', (event)) => {
-//         if (event.target.value === productID) {
-//             myVotes++;
-//         }
+let totalClicks = 0;
+let live = true;
+
+// const masterProductSet = new ProductSet(products);
+
+// const handleUserChoice = (event) => {
+//     if (!live) return;
+//     const radioElement = event.target;
+//     choiceContainer.classList.add('reveal');
+//     radioElement.parentNode.classList.add('choice');
+
+//     totalClicks++;
+
+//     const userChoice = products.id === radioElement.value;
+
+//     if (userChoice) {
+//         masterProductSet.addById(userChoice.id);
 //     }
+
+//     else { (totalClicks < 1), 
+//     removeProductById(products.id)};
+// };
+
+// for (let i = 0; i < radioInputs.length; i++) {
+//     const radioInput = radioInputs[i];
+//     radioInput.addEventListener('input', handleUserChoice);
 // }
+
+// nextButton.addEventListener('click', () => {
+//     if (masterProductSet.hasProdcuts()) {
+//         displayFinalResults();
+//         return;
+//     }
+// });
+
 while (randomProduct1 === randomProduct2) {
     randomProduct2 = products.getRandomProducts();
 }
-while (randomProduct3 === products.getRandomProducts() || randomProduct3 === randomProduct1) {
+while (randomProduct3 === products.getRandomProducts() || randomProduct3 === randomProduct1 || randomProduct3 === randomProduct2) {
     randomProduct3 = products.getRandomProducts();
 }
 
@@ -29,16 +60,7 @@ img1.src = randomProduct1.image;
 img2.src = randomProduct2.image;
 img3.src = randomProduct3.image;
 
-        // productImageTags.forEach((imageTag, i) => {
-        //     if (i === 0) {
-        //         imageTag.src = randomProduct.image;
-        //     } else if (i === 1) {
-        //         imageTag.src = randomProduct2.image;
-        //     }
-        //     else if (i === 2) {
-        //         imageTag.src = randomProduct3.image;
-        //     }
-        // });
+        
 productRadioTags.forEach((radioTag, i) => {
     if (i === 0) {
         radioTag.value = randomProduct1.id;
@@ -49,4 +71,3 @@ productRadioTags.forEach((radioTag, i) => {
     }
     console.log(radioTag.value);
 });
-    
