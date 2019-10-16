2e3cd3c9cd82bbdf076c5dd9a4ebc9ca32a9ed30
Erika Foin <erika.foin@gmail.com>
2019-10-15 17:55:39 -0700

    fixing the while loops

--- app.js
+++ app.js
@@ -1,37 +1,47 @@
 import { productData } from './products.js';
-import { ProductArray } from './ProductArray.js';
+import { ProductData } from '/bus-mall/ProductArray.js';
 
-const productImageTags = document.querySelectorAll('img');
+const img1 = document.getElementById('img-1');
+const img2 = document.getElementById('img-2');
+const img3 = document.getElementById('img-3');
 const productRadioTags = document.querySelectorAll('input');
-const productName = document.getElementById('product');
-const products = new ProductArray(productData);
-
-const generateProducts = () => {
-    const randomProduct = products.getRandomProduct();
-    let randomProduct2 = products.getRandomProduct(); {
-    while (randomProduct2 === products.getRandomProduct();
-}
+const productName = document.querySelectorAll('product-name');
+const products = new ProductData(productData);
+let randomProduct1 = products.getRandomProduct();
+let randomProduct2 = products.getRandomProduct(); 
 let randomProduct3 = products.getRandomProduct();
-while (randomProduct3 === products.getRandomProduct() || randomProduct3.id === randomProduct.id) {
 
-productImageTags.forEach((imageTag, i) => {
-if (i === 0) {
-    imageTag.src = randomProduct.image;
-} else if (i === 1) {
-    imageTag.src = randomProduct2.image;
-}
-else if (i === 2) {
-    imageTag.src = randomProduct3.image;
-}
-});
-productRadioTags.forEach((radioTag, i) => {
-    if (i === 0) {
-        radioTag.value = randomProduct.id;
-    } else if (i === 1) {
-        radioTag.value = randomProduct2.id;
-    } else if (i === 2) {
-        radioTag.value = randomProduct3.id;
+        while (randomProduct1 === randomProduct2) {
+            randomProduct2 = products.getRandomProduct();
+        }
+        while (randomProduct3 === products.getRandomProduct() || randomProduct3 === randomProduct1) {
+            randomProduct3 = products.getRandomProduct();
+        }
+
+        img1.src = getRandomProduct1.image;
+        img2.src = getRandomProduct2.image;
+        img3.src = getRandomProduct3.image;
+
+        // productImageTags.forEach((imageTag, i) => {
+        //     if (i === 0) {
+        //         imageTag.src = randomProduct.image;
+        //     } else if (i === 1) {
+        //         imageTag.src = randomProduct2.image;
+        //     }
+        //     else if (i === 2) {
+        //         imageTag.src = randomProduct3.image;
+        //     }
+        // });
+        productRadioTags.forEach((radioTag, i) => {
+            if (i === 0) {
+                radioTag.value = randomProduct.id;
+            } else if (i === 1) {
+                radioTag.value = randomProduct2.id;
+            } else if (i === 2) {
+                radioTag.value = randomProduct3.id;
+            }
+            console.log(radioTag.value);
+        });
     }
-    console.log(radioTag.value);
-    });
+    generateProducts();
 };
